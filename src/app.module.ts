260ca160c33c027de67ce46c020b8291d40d4e59
Valentine117 <shmidas117@naver.com>
2024-01-11 19:53:58 +0900

    feat: make module payments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PaymentsModule } from './payments/payments.module';
+import { ControllersController } from './src/payments/controllers/controllers.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [PaymentsModule],
+  controllers: [AppController, ControllersController],
   providers: [AppService],
 })
 export class AppModule {}
